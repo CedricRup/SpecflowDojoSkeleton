e883454f32cd236ebc77f17f4f03b8dde017e87a
Cedric Rup <cedric.rup@gmail.com>
2015-03-26 13:46:55 +0100

    Add Specflow + MSTest

---
+++ SpecMSTest/DummySteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ApiClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using TechTalk.SpecFlow;
+using WebApi.Controllers;
+
+namespace Specs
+{
+    [Binding]
+    public class DummySteps
+    {
+        private Dummy _dummy;
+
+        [When(@"je récupère un Dummy")]
+        public void QuandJeRecupereUnDummy()
+        {
+            _dummy = new ApiClient.ApiClient().GetDummy();
+        }
+
+        [Then(@"le nom du Dummy devrait être '(.*)'")]
+        public void AlorsLeNomDuDummyDevraitEtre(string dummyName)
+        {
+            Assert.AreEqual(dummyName, _dummy.Name);
+        }
+    }
+}
---
+++ SpecMSTest/Hooks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+using TechTalk.SpecFlow;
+using WebApi;
+
+namespace Specs
+{
+    [Binding]
+    public class Hooks
+    {
+        private static IDisposable _server;
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            _server = WebApp.Start<StartupSpecFlow>("http://localhost:12345");
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            if (_server != null)
+            {
+                _server.Dispose();
+            }
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+        }
+    }
+
+    public class StartupSpecFlow
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            new Startup().Configuration(app, configuration);
+        }
+    }
+}
