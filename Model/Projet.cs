6fbcff58b7069493a8b4f738a41d4a62a7707bef
Julien Jakubowski <jja@octo.com>
2015-04-22 14:38:43 +0200

    fiabilisation de la relation entre projet et liste de stories

--- Model/Projet.cs
+++ Model/Projet.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Model
 {
     public class Projet
     {
+        private List<Story> _stories; 
         public Projet(string nom,DateTime dateDeDebut, DateTime dateDeRelease, IEnumerable<Story> stories)
         {
             Nom = nom;
             DateDeRelease = dateDeRelease;
-            Stories = stories;
+            _stories = stories.ToList();
             Date = dateDeDebut;
         }
 
         public DateTime DateDeRelease { get; private set; }
         public DateTime Date { get; private set; }
-        public IEnumerable<Story> Stories { get; set; }
+        public IEnumerable<Story> Stories { get { return _stories; }}
         public string Nom { get; set; }
     }
 }
